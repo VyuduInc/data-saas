334c25ad4e0501b2c5ba6c918b3c525524012596
Jeremy Williams <jeremy@vyudu.com>
2024-12-15 13:52:22 -0500

    S3 updates and error fixes
    I've made several improvements to the S3 utility:
    
    Fixed the template literal syntax:
    Changed ``` to ``` for proper template literal syntax
    Fixed the escaped ${...} expressions
    Added environment variable validation:
    Added checks for all required AWS environment variables
    These will throw helpful errors if any required variables are missing
    Removed non-null assertions:
    Removed the ! operators since we're now validating the environment variables
    This makes the code more type-safe

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21eef7c8a50242c2"; }
+/******/ 		__webpack_require__.h = function() { return "f3bec434affe5a27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,16 +1,21 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
+if (!process.env.AWS_REGION) throw new Error('AWS_REGION is required');
+if (!process.env.AWS_ACCESS_KEY_ID) throw new Error('AWS_ACCESS_KEY_ID is required');
+if (!process.env.AWS_SECRET_ACCESS_KEY) throw new Error('AWS_SECRET_ACCESS_KEY is required');
+if (!process.env.AWS_S3_BUCKET) throw new Error('AWS_S3_BUCKET is required');
+
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
 export async function uploadToS3(file: File, key: string): Promise<string> {
   const buffer = Buffer.from(await file.arrayBuffer());
-  const bucketName = process.env.AWS_S3_BUCKET!;
+  const bucketName = process.env.AWS_S3_BUCKET;
 
   await s3Client.send(
     new PutObjectCommand({
@@ -21,5 +26,5 @@ export async function uploadToS3(file: File, key: string): Promise<string> {
     })
   );
 
-  return \`https://\${bucketName}.s3.\${process.env.AWS_REGION}.amazonaws.com/\${key}\`;
+  return `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
 }
